4b67716773093b88f8a42ac00ba414ebe2b4ad94
Gaston Tognola <tognola08@gmail.com>
2021-10-30 22:29:43 -0300

    Primer test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    let caracterisitica;
+    let numero;
+    const url = "https://api.whatsapp.com/send?phone="
+    
+    function send(e){
+        e.preventDefault();
+        console.log(caracterisitica + numero)
+        window.open(url+caracterisitica + numero, '_blank').focus();
+    }
+
+</script>
+
+<svelte:head>
+    <title>WhatsappeA</title>
+</svelte:head>
+
+<main>
+    <h1>WhatsappeA</h1>
+
+    <form action="">
+        <select name="" id="" bind:value={caracterisitica}>
+            <option value="+549">ARG (+54)</option>
+        </select>
+        <input type="number" bind:value={numero}>
+        <button type="submit" on:click={send}> Enviar</button>
+    </form>
+</main>
+
+
+<style>
+    main{
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        background-color: rgb(0, 90, 173);
+        font-family: sans-serif;
+    }
+
+    h1{
+        color: white;
+        position: relative;
+        margin-bottom: 1em;
+    }
+
+    select{
+        padding: 0.5em;
+        border-radius: .5em;
+    }
+
+    input {
+        padding: 0.5em;
+        border-radius: .5em;
+    }
+</style>
