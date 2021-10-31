8c0c3c0c0409cc229508115161515b6f632ccc2a
Gaston Tognola <tognola08@gmail.com>
2021-10-31 10:43:45 -0300

    added countries

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,8 +1,17 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-	const dispatch = createEventDispatcher();
+	import { onMount } from 'svelte';
 
-	let caracteristica;
+	const dispatch = createEventDispatcher();
+    let caracteristica;
+
+    onMount( () => {
+		caracteristica = localStorage.getItem('areacode')
+		? localStorage.getItem('areacode')
+		: '+549';
+	});
+
+	
 	let numero;
 	let showName = false;
 
@@ -15,13 +24,25 @@
 		e.preventDefault();
 		showName = !showName;
 	}
+
+	function saveOption() {
+		localStorage.setItem('areacode', caracteristica);
+	}
 </script>
 
 <form action="">
-	<select class="input" name="" id="" bind:value={caracteristica}>
+	<select class="input" name="" id="" bind:value={caracteristica} on:change={saveOption}>
 		<option value="+549">ARG (+54)</option>
+		<option value="+55">BRA (+55)</option>
+		<option value="+569">CHI (+56)</option>
+		<option value="+579">COL (+57)</option>
 	</select>
-	<input class="input" type="number" bind:value={numero} placeholder="Número de teléfono (Sin 0 y sin 15)" />
+	<input
+		class="input"
+		type="number"
+		bind:value={numero}
+		placeholder="Número de teléfono (Sin 0 y sin 15)"
+	/>
 	<button type="submit" on:click={sendWhatsApp}>Enviar</button>
 	<!-- button on:click={saveContact}> Guardar</button -->
 </form>
@@ -35,26 +56,26 @@
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: center;
-        align-items: center;
+		align-items: center;
 	}
 	.input {
 		padding: 0.5em;
 		border-radius: 0.5em;
-        margin: 0 0.5em;
-        border: 0;
+		margin: 0 0.5em;
+		border: 0;
 	}
 
-    select.input{
-        flex-grow: 1;
-    }
+	select.input {
+		flex-grow: 1;
+	}
 
-    input.input{
-        flex-grow: 2;
-    }
+	input.input {
+		flex-grow: 2;
+	}
 
 	button {
 		padding: 0.5em;
-        margin: 1em 0.5em;
-        flex-grow: 1;
+		margin: 1em 0.5em;
+		flex-grow: 1;
 	}
 </style>
