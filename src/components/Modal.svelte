35f92783705675ee42baea7bcc4f0cbf421f60b0
Gaston Tognola <tognola08@gmail.com>
2022-02-06 22:21:56 -0300

    fix/ error message

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -3,16 +3,20 @@
 </script>
 
 <div>
-    error
+    Ingresa un número sin letras ni espacios.
 </div>
 
 <style>
     div {
         position: fixed;
-        top: 50%;
+        top: 10%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 50vw;
         height: fit-content;
+        text-align: center;
+        background-color: rgba(255,255,255,0.5);
+        padding: 2em;
+        border: 5px solid rgba(255, 0, 0, 0.5);
+        border-radius: 1em;
     }
 </style>
